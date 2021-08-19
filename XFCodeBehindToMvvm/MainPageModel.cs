8d5f4678f83a36ed62effdb60d0c8fd90a7caeaf
Matthias/DESKTOP-8K0IGUE <matthias.reynders3@gmail.com>
2021-08-19 17:26:52 +0200

    Add project files.

---
+++ XFCodeBehindToMvvm/MainPageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFCodeBehindToMvvm
+{
+    public class MainPageModel : INotifyPropertyChanged
+    {
+        public string EntryValue { get; set; }
+        public string TitleValue { get; set; }
+
+        public Command ChangeTitleCommand {  get; set; }
+
+        public MainPageModel()
+        {
+            ChangeTitleCommand = new Command(() =>
+            {
+                TitleValue = EntryValue;
+                OnPropertyChanged(nameof(TitleValue));
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
